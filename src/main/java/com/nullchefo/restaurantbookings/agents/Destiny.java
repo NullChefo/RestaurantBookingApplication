1e37eb749496851cefdcd495c8d2905540748c03
Stefan Kehayov <stefankehayov>
2024-06-06 15:44:43 +0300

    Did not fucked up yet, but added new ontology

---
+++ src/main/java/com/nullchefo/restaurantbookings/agents/Destiny.java
@@ -0,0 +1,13 @@
+package com.nullchefo.restaurantbookings.agents;
+
+import jade.core.Agent;
+
+public class Destiny extends Agent {
+
+	// spawns clients and reservations
+
+	@Override
+	protected void setup() {
+
+	}
+}
